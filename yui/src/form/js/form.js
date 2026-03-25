d272f5aee13cfc43188e5a6aecce2c4739dc64e0
Toma Andrei Gabriel <56647464+tandreigaabriel@users.noreply.github.com>
2026-03-25 13:30:21 +0000

    Update form.js

--- yui/src/form/js/form.js
+++ yui/src/form/js/form.js
@@ -2,150 +2,182 @@
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+// the Free Software Foundation.
 
 /**
- * Legacy YUI module for the Stripe availability condition form editor.
+ * AMD module for the Stripe availability condition form editor.
  *
- * This file is retained for reference only. The active implementation has
- * been migrated to the AMD module at amd/src/form.js, loaded via
- * frontend.php::include_javascript().
+ * This module defines the UI for configuring the Stripe payment restriction.
+ * It replaces the legacy YUI module and integrates with core_availability.
  *
- * @module     moodle-availability_stripepayment-form
+ * Language strings are pre-loaded by frontend.php via js_call_amd and passed
+ * to init() as the second argument, ensuring synchronous access in getNode().
+ *
+ * @module     availability_stripepayment/form
  * @copyright  2025 Andrei Toma <https://www.tagwebdesign.co.uk>
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-M.availability_stripepayment = M.availability_stripepayment || {};
 
-/**
- * @class M.availability_stripepayment.form
- * @extends M.core_availability.plugin
- */
-M.availability_stripepayment.form = Y.Object(M.core_availability.plugin);
+define([], function() {
+    'use strict';
 
-/**
- * Initialises this plugin.
- *
- * @method initInner
- * @param {Array} currencies Array of currency_code => localised string
- */
-M.availability_stripepayment.form.initInner = function(currencies) {
-    this.currencies = currencies;
-};
+    return {
 
-M.availability_stripepayment.form.getNode = function(json) {
-    var selected_string = '';
-    var currencies_options = '';
-    for (var curr in this.currencies) {
-        if (json.currency === curr) {
-            selected_string = ' selected="selected" ';
-        } else {
-            selected_string = '';
+        /**
+         * Initialise the Stripe availability form.
+         *
+         * @param {Object} currencies Map of currency codes to names
+         * @param {Object} strings    Map of label strings pre-loaded from PHP
+         */
+        init: function(currencies, strings) {
+
+            // Ensure namespace exists.
+            M.availability_stripepayment = M.availability_stripepayment || {};
+
+            /**
+             * Form object used by Moodle core_availability.
+             */
+            M.availability_stripepayment.form = {
+
+                currencies: currencies,
+                strings: strings,
+                addedEvents: false,
+
+                /**
+                 * Build and return the form node synchronously.
+                 *
+                 * @param {Object} json Existing condition data
+                 * @return {HTMLElement}
+                 */
+                getNode: function(json) {
+                    var self = this;
+
+                    // Build currency dropdown options.
+                    var options = '';
+                    for (var code in self.currencies) {
+                        if (!Object.prototype.hasOwnProperty.call(self.currencies, code)) {
+                            continue;
+                        }
+
+                        var selected = (json.currency === code) ? ' selected="selected"' : '';
+                        options += '<option value="' + code + '"' + selected + '>'
+                            + self.currencies[code] + '</option>';
+                    }
+
+                    // Build HTML form structure using pre-loaded strings.
+                    var html = '<span><div class="container-fluid">'
+
+                        // Currency
+                        + '<div class="row mt-3">'
+                        + '<div class="col"><label for="stripecurrency">' + self.strings.currency + '</label></div>'
+                        + '<div class="col">'
+                        + '<select class="form-control" name="currency" id="stripecurrency">'
+                        + options + '</select>'
+                        + '</div></div>'
+
+                        // Amount
+                        + '<div class="row mt-3">'
+                        + '<div class="col"><label for="stripeamount">' + self.strings.amount + '</label></div>'
+                        + '<div class="col">'
+                        + '<input class="form-control" name="amount" type="text" id="stripeamount" />'
+                        + '</div></div>'
+
+                        // Item name
+                        + '<div class="row mt-3">'
+                        + '<div class="col"><label for="stripeitemname">' + self.strings.itemname + '</label></div>'
+                        + '<div class="col">'
+                        + '<input class="form-control" name="itemname" type="text" id="stripeitemname" />'
+                        + '</div></div>'
+
+                        + '</div></span>';
+
+                    // Convert HTML string to DOM node.
+                    var wrapper = document.createElement('div');
+                    wrapper.innerHTML = html;
+                    var node = wrapper.firstChild;
+
+                    // Populate existing values.
+                    if (json.amount !== undefined) {
+                        node.querySelector('input[name=amount]').value = json.amount;
+                    }
+                    if (json.itemname !== undefined) {
+                        node.querySelector('input[name=itemname]').value = json.itemname;
+                    }
+
+                    // Attach change listeners once.
+                    if (!self.addedEvents) {
+                        self.addedEvents = true;
+
+                        document.addEventListener('change', function(e) {
+                            var target = e.target;
+
+                            if (!target.closest('.availability_stripepayment')) {
+                                return;
+                            }
+
+                            if (target.matches('select[name=currency]')
+                                || target.matches('input[name=amount]')
+                                || target.matches('input[name=itemname]')) {
+
+                                // Notify Moodle form to update state.
+                                M.core_availability.form.update();
+                            }
+                        });
+                    }
+
+                    return node;
+                },
+
+                /**
+                 * Populate JSON value from form inputs.
+                 *
+                 * @param {Object} value
+                 * @param {HTMLElement} node
+                 */
+                fillValue: function(value, node) {
+                    value.currency = node.querySelector('select[name=currency]').value;
+                    value.amount = this.getValue('amount', node);
+                    value.itemname = node.querySelector('input[name=itemname]').value;
+                },
+
+                /**
+                 * Parse numeric input safely.
+                 *
+                 * @param {string} field
+                 * @param {HTMLElement} node
+                 * @return {number|string}
+                 */
+                getValue: function(field, node) {
+                    var value = node.querySelector('input[name=' + field + ']').value;
+
+                    // Validate number format.
+                    if (!(/^[0-9]+([.,][0-9]+)?$/.test(value))) {
+                        return value;
+                    }
+
+                    return parseFloat(value.replace(',', '.'));
+                },
+
+                /**
+                 * Validate form values and push errors.
+                 *
+                 * @param {Array} errors
+                 * @param {HTMLElement} node
+                 */
+                fillErrors: function(errors, node) {
+                    var value = {};
+                    this.fillValue(value, node);
+
+                    if ((value.amount !== undefined && typeof value.amount === 'string')
+                        || value.amount <= 0) {
+                        errors.push('availability_stripepayment:error_amount_required');
+                    }
+
+                    if (value.itemname === '') {
+                        errors.push('availability_stripepayment:error_itemname_required');
+                    }
+                }
+            };
         }
-        currencies_options += '<option value="' + curr + '" ' + selected_string + ' >';
-        currencies_options += this.currencies[curr];
-        currencies_options += '</option>';
-    }
-
-    var html = '<div class="container-fluid">';
-    html += '<div class="row mt-3">';
-    html += '<div class="col"><label for="stripecurrency">';
-    html += M.util.get_string('currency', 'availability_stripepayment');
-    html += '</label></div><div class="col"><select class="form-control" name="currency" id="stripecurrency" />' +
-            currencies_options + '</select></div></div>';
-
-    html += '<div class="row mt-3">';
-    html += '<div class="col"><label for="stripeamount">';
-    html += M.util.get_string('amount', 'availability_stripepayment');
-    html += '</label></div><div class="col"><input class="form-control" name="amount" type="text" id="stripeamount" /></div></div>';
-
-    html += '<div class="row mt-3">';
-    html += '<div class="col"><label for="stripeitemname">';
-    html += M.util.get_string('itemname', 'availability_stripepayment');
-    html += '</label></div><div class="col"><input class="form-control" name="itemname"' +
-            ' type="text" id="stripeitemname" /></div></div>';
-    html += '</div>';
-
-    var node = Y.Node.create('<span>' + html + '</span>');
-
-    // Set initial values based on the value from the JSON data in Moodle
-    // database. This will have values undefined if creating a new one.
-    if (json.amount) {
-        node.one('input[name=amount]').set('value', json.amount);
-    }
-    if (json.itemname) {
-        node.one('input[name=itemname]').set('value', json.itemname);
-    }
-
-    // Add event handlers (first time only).
-    if (!M.availability_stripepayment.form.addedEvents) {
-        M.availability_stripepayment.form.addedEvents = true;
-
-        var root = Y.one('.availability-field');
-        root.delegate('change', function() {
-            M.core_availability.form.update();
-        }, '.availability_stripepayment select[name=currency]');
-
-        root.delegate('change', function() {
-                // The key point is this update call. This call will update
-                // the JSON data in the hidden field in the form, so that it
-                // includes the new value of the checkbox.
-                M.core_availability.form.update();
-        }, '.availability_stripepayment input');
-    }
-
-    return node;
-};
-
-M.availability_stripepayment.form.fillValue = function(value, node) {
-    // This function gets passed the node (from above) and a value
-    // object. Within that object, it must set up the correct values
-    // to use within the JSON data in the form. Should be compatible
-    // with the structure used in the __construct and save functions
-    // within condition.php.
-    value.currency = node.one('select[name=currency]').get('value');
-    value.amount = this.getValue('amount', node);
-    value.itemname = node.one('input[name=itemname]').get('value');
-};
-
-/**
- * Gets the numeric value of an input field. Supports decimal points (using
- * dot or comma).
- *
- * @method getValue
- * @return {Number|String} Value of field as number or string if not valid
- */
-M.availability_stripepayment.form.getValue = function(field, node) {
-    // Get field value.
-    var value = node.one('input[name=' + field + ']').get('value');
-
-    // If it is not a valid positive number, return false.
-    if (!(/^[0-9]+([.,][0-9]+)?$/.test(value))) {
-        return value;
-    }
-
-    // Replace comma with dot and parse as floating-point.
-    var result = parseFloat(value.replace(',', '.'));
-    return result;
-};
-
-M.availability_stripepayment.form.fillErrors = function(errors, node) {
-    var value = {};
-    this.fillValue(value, node);
-
-    if ((value.amount !== undefined && typeof(value.amount) === 'string') || value.amount <= 0 ) {
-        errors.push('availability_stripepayment:error_amount_required');
-    }
-    if (value.itemname === '') {
-        errors.push('availability_stripepayment:error_itemname_required');
-    }
-};
+    };
+});
